cd3eb73336fd2b03abe747cc980725113f96f5c0
Joe Tay <64293979+twahidin@users.noreply.github.com>
2023-04-10 13:05:39 +0800

    Update dialogic_agent.py

--- dialogic_agent.py
+++ dialogic_agent.py
@@ -18,7 +18,7 @@ from typing import List, Dict
 from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from agent_tools import wikipedia_to_json_string, google_search_crawl, document_search, google_search_serp, bing_search_internet
+from agent_tools import wikipedia_to_json_string, document_search, google_search_serp, bing_search_internet
 openai.api_key  = st.session_state.api_key
 
 @st.cache_resource
